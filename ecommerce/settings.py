797222020c37fd217de0a7c1176fa6afc9f94986
pavel <krav4enko.paw@gmail.com>
2021-05-09 17:35:23 +0300

    Connect Postgre in project

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -74,10 +74,23 @@ WSGI_APPLICATION = 'ecommerce.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ecommercedb',
+        'USER': 'postgres',
+        'PASSWORD':'123123',
+        'HOST':'localhost',
+        'PORT': 5432
+
+
+
+
     }
+
 }
 
 
